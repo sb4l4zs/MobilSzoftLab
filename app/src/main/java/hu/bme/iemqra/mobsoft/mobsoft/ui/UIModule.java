c98c829163e1df2ebbe69175c2d599c491defbcf
Suru Balazs <surubalazs@gmail.com>
2017-03-20 11:24:37 +0100

    provided new ui's for UIModule

--- app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/ui/UIModule.java
+++ app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/ui/UIModule.java
@@ -7,7 +7,10 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import hu.bme.iemqra.mobsoft.mobsoft.ui.details.DetailsPresenter;
+import hu.bme.iemqra.mobsoft.mobsoft.ui.login.LoginPresenter;
 import hu.bme.iemqra.mobsoft.mobsoft.ui.main.MainPresenter;
+import hu.bme.iemqra.mobsoft.mobsoft.ui.registration.RegisterPresenter;
 
 /**
  * Created by mobsoft on 2017. 03. 20..
@@ -30,5 +33,23 @@ public class UIModule {
     @Singleton
     public MainPresenter provideMainPresenter() {
         return new MainPresenter();
+    }
+
+    @Provides
+    @Singleton
+    public LoginPresenter provideLoginPresenter() {
+        return new LoginPresenter();
+    }
+
+    @Provides
+    @Singleton
+    public RegisterPresenter provideRegisterPresenter() {
+        return new RegisterPresenter();
+    }
+
+    @Provides
+    @Singleton
+    public DetailsPresenter provideDetailsPresenter() {
+        return new DetailsPresenter();
     }
 }
