f8f73cfe64094c6a2a94f87ed6bd455a81df12f8
Suru Balazs <surubalazs@gmail.com>
2017-03-20 10:08:05 +0100

    main presenter

---
+++ app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/ui/Presenter.java
@@ -0,0 +1,17 @@
+package hu.bme.iemqra.mobsoft.mobsoft.ui;
+
+/**
+ * Created by mobsoft on 2017. 03. 20..
+ */
+
+public abstract class Presenter<S> {
+    protected S screen;
+
+    public void attachScreen(S screen) {
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+}
---
+++ app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/ui/main/MainScreen.java
@@ -0,0 +1,9 @@
+package hu.bme.iemqra.mobsoft.mobsoft.ui.main;
+
+/**
+ * Created by mobsoft on 2017. 03. 20..
+ */
+
+public interface MainScreen {
+    void showMessage(String text);
+}
